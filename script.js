c05b48008b57a002d047cf9ded5e1b5aea11e173
xaIex <141204055+xaIex@users.noreply.github.com>
2024-02-23 18:34:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function validateForms(){
+	let input1 = document.getElementById("email").value;
+    
+    if(input1 == "yahoo@com"){
+        alert("Not a valid email!");
+        DocumentFragment.getElementById("pwd").value = "";
+        return false;
+    }
+	
+	let input2 = document.getElementById("pwd").value;
+    let pwPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/; //regex
+    if(!pwPattern.test(input2)){
+        alert("Please use at least one upper and lower case, one digit and at least 6 characters long");
+        document.getElementById("pwd").value = ""; //clear field
+        return false;
+    }
+	
+	let input3 = document.getElementById("phone").value;
+    let phonePattern = /^\d{10}$/; // at least 10 digits
+    if(!phonePattern.test(input3)){
+        alert("Invalid Phone number");
+        document.getElementById("phone").value = "";
+        return false;
+    }
+    
+	let input4 = document.getElementById("zip").value;
+    let zipPattern = /^\d{5}(?:-\d{4})?$/;
+    if(!zipPattern.test(input4)){
+        alert("Invalid Zipcode");
+        document.getElementById("zip").value = "";
+        return false;
+    }
+ return true;
+   
+}
